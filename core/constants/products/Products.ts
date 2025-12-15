418f2430ca2988f17c9251e679a0a397cd2cec05
khatereh <khaterehnaseri444@gmail.com>
2025-12-15 12:58:50 +0330

    feat:crate items array

---
+++ core/constants/products/Products.ts
@@ -0,0 +1,251 @@
+interface ProductsType {
+    id: number
+    name: string
+    img: string
+    price: number
+    category: string
+    images: string[]
+}
+
+export const products: ProductsType[] = [
+    {
+        id: 1,
+        name: 'MAC lip stick',
+        img: '/assets/images/item1.jpg',
+        price: 30,
+        category: 'lipMakeup',
+        images: [
+            '/assets/images/itemDetail1-1.jpg',
+            '/assets/images/itemDetail1-2.jpg',
+            '/assets/images/itemDetail1-3.jpg',
+        ]
+    },
+    {
+        id: 2,
+        name: 'SHIGLAM primer',
+        img: '/assets/images/item2.jpg',
+        price: 45,
+        category: 'faceMakeup',
+        images: [
+            '/assets/images/itemDetail2-1.jpg',
+            '/assets/images/itemDetail2-2.jpg',
+            '/assets/images/itemDetail2-1.jpg',
+        ]
+    },
+    {
+        id: 3,
+        name: 'Dior eyeliner',
+        img: '/assets/images/item3.jpg',
+        price: 27,
+        category: 'faceMakeup',
+        images: [
+            '/assets/images/item3.jpg',
+            '/assets/images/item3.jpg',
+            '/assets/images/item3.jpg',
+        ]
+    },
+    {
+        id: 4,
+        name: 'Ordinary serum face',
+        img: '/assets/images/item4.jpg',
+        price: 65,
+        category: 'skinCare',
+        images: [
+            '/assets/images/itemDetail4-1.jpg',
+            '/assets/images/itemDetail4-2.jpg',
+            '/assets/images/itemDetail4-3.jpg',
+        ]
+    },
+    {
+        id: 5,
+        name: 'Dior lipstick',
+        img: '/assets/images/item5.jpg',
+        price: 99,
+        category: 'lipMakeup',
+        images: [
+            '/assets/images/itemDetail5-1.jpg',
+            '/assets/images/itemDetail5-2.jpg',
+            '/assets/images/itemDetail5-3.jpg',
+        ]
+    },
+    {
+        id: 6,
+        name: 'TIR TIR red cushon',
+        img: '/assets/images/item6.jpg',
+        price: 87,
+        category: 'faceMakeup',
+        images: [
+            '/assets/images/itemDetail6-1.jpg',
+            '/assets/images/itemDetail6-2.jpg',
+            '/assets/images/itemDetail6-3.jpg',
+        ]
+    },
+    {
+        id: 7,
+        name: 'Kiko eye shadow',
+        img: '/assets/images/item7.jpg',
+        price: 320,
+        category: 'eyeMakeup',
+        images: [
+            '/assets/images/itemDetail7-1.jpg',
+            '/assets/images/itemDetail7-2.jpg',
+            '/assets/images/itemDetail7-3.jpg',
+        ]
+    },
+    {
+        id: 8,
+        name: 'Simple face wash',
+        img: '/assets/images/item8.jpg',
+        price: 55,
+        category: 'skinCare',
+        images: [
+            '/assets/images/itemDetail8-1.jpg',
+            '/assets/images/itemDetail8-2.jpg',
+            '/assets/images/itemDetail8-3.jpg',
+        ]
+    },
+    {
+        id: 9,
+        name: 'Kiko lip gloss',
+        img: '/assets/images/item9.jpg',
+        price: 89,
+        category: 'lipMakeup',
+        images: [
+            '/assets/images/itemDetail9-1.jpg',
+            '/assets/images/itemDetail9-2.jpg',
+            '/assets/images/itemDetail9-3.jpg',
+        ]
+    },
+    {
+        id: 10,
+        name: 'Essence mascara',
+        img: '/assets/images/item10.jpg',
+        price: 112,
+        category: 'eyeMakeup',
+        images: [
+            '/assets/images/itemDetail10-1.jpg',
+            '/assets/images/itemDetail10-2.jpg',
+            '/assets/images/itemDetail10-3.jpg',
+        ]
+    },
+    {
+        id: 11,
+        name: 'Shiglam blusher',
+        img: '/assets/images/item11.jpg',
+        price: 65,
+        category: 'faceMakeup',
+        images: [
+            '/assets/images/itemDetail11-1.jpg',
+            '/assets/images/itemDetail11-2.jpg',
+            '/assets/images/itemDetail11-3.jpg',
+        ]
+    },
+    {
+        id: 12,
+        name: 'Qlinique face mask',
+        img: '/assets/images/item12.jpg',
+        price: 21,
+        category: 'skinCare',
+        images: [
+            '/assets/images/itemDetail12-1.jpg',
+            '/assets/images/itemDetail12-2.jpg',
+            '/assets/images/itemDetail12-3.jpg',
+        ]
+    },
+    {
+        id: 13,
+        name: 'Qlinique powder',
+        img: '/assets/images/item13.jpg',
+        price: 130,
+        category: 'faceMakeup',
+        images: [
+            '/assets/images/itemDetail13-1.jpg',
+            '/assets/images/itemDetail13-2.jpg',
+            '/assets/images/itemDetail13-3.jpg',
+        ]
+    },
+    {
+        id: 14,
+        name: 'YSL lip stick',
+        img: '/assets/images/item14.jpg',
+        price: 47,
+        category: 'lipMakeup',
+        images: [
+            '/assets/images/itemDetail14-1.jpg',
+            '/assets/images/itemDetail14-2.jpg',
+            '/assets/images/itemDetail14-3.jpg',
+        ]
+    },
+    {
+        id: 15,
+        name: 'Nivea lip balm',
+        img: '/assets/images/item15.jpg',
+        price: 12,
+        category: 'skinCare',
+        images: [
+            '/assets/images/itemDetail15-1.jpg',
+            '/assets/images/itemDetail15-2.jpg',
+            '/assets/images/itemDetail15-3.jpg',
+        ]
+    },
+    {
+        id: 16,
+        name: 'Nars mascara',
+        img: '/assets/images/item16.jpg',
+        price: 64,
+        category: 'eyeMakeup',
+        images: [
+            '/assets/images/itemDetail16-1.jpg',
+            '/assets/images/itemDetail16-2.jpg',
+            '/assets/images/itemDetail16-3.jpg',
+        ]
+    },
+    {
+        id: 17,
+        name: 'Channel lip stick',
+        img: '/assets/images/item17.jpg',
+        price: 97,
+        category: 'lipMakeup',
+        images: [
+            '/assets/images/itemDetail17-1.jpg',
+            '/assets/images/itemDetail17-2.jpg',
+            '/assets/images/itemDetail17-3.jpg',
+        ]
+    },
+    {
+        id: 18,
+        name: 'MAC powder blush',
+        img: '/assets/images/item18.jpg',
+        price: 114,
+        category: 'faceMakeup',
+        images: [
+            '/assets/images/itemDetail18-1.jpg',
+            '/assets/images/itemDetail18-2.jpg',
+            '/assets/images/itemDetail18-3.jpg',
+        ]
+    },
+    {
+        id: 19,
+        name: 'Elf eye liner',
+        img: '/assets/images/item19.jpg',
+        price: 43,
+        category: 'eyeMakeup',
+        images: [
+            '/assets/images/itemDetail19-1.jpg',
+            '/assets/images/itemDetail19-2.jpg',
+            '/assets/images/itemDetail19-3.jpg',
+        ]
+    },
+    {
+        id: 20,
+        name: 'Pixi glow tonic',
+        img: '/assets/images/item20.jpg',
+        price: 12,
+        category: 'skinCare',
+        images: [
+            '/assets/images/itemDetail20-1.jpg',
+            '/assets/images/itemDetail20-2.jpg',
+            '/assets/images/itemDetail20-3.jpg',
+        ]
+    },
+]
