56e8afa256ac64175bc4406ed544f9ecec880409
khatereh <khaterehnaseri444@gmail.com>
2025-12-30 11:09:03 +0330

    feat:set all catch routing for filter page

--- components/ui/organisms/productsOrganisms/ProductsOrganisms.tsx
+++ components/ui/organisms/productsOrganisms/ProductsOrganisms.tsx
@@ -3,14 +3,18 @@ import P from "../../atom/CustomP/P";
 import Products from "../../molecules/products/Products";
 import Filter from "../../molecules/filter/Filter";
 import SearchBar from "../../molecules/searchBar/SearchBar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { products } from "@/core/constants/products/Products";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
 
 function ProductsOrganisms() {
-    const [valueInput, setValueInput] = useState<string>('')
-    const [chosenBrand, setChosenBrand] = useState<string[]>([])
-    const [chosenCategory, setChosenCategory] = useState<string[]>([])
-    const [priceRange, setPriceRange] = useState<[number, number]>([0, 200])
+    const searchParams = useSearchParams();
+    const router = useRouter();
+    const [valueInput, setValueInput] = useState<string>(searchParams.get('search') || '')
+    const [chosenBrand, setChosenBrand] = useState<string[]>(searchParams.get('brand')?.split(',') || [])
+    const [chosenCategory, setChosenCategory] = useState<string[]>(searchParams.get('category')?.split(',') || [])
+    const [priceRange, setPriceRange] = useState<[number, number]>([Number(searchParams.get('min') || 0), Number(searchParams.get('max') || 200)])
     const filterProducts = products.filter((item) => {
         const filterSeaarch = item.name.toLowerCase().includes(valueInput.toLowerCase())
         const filterBrands = chosenBrand.length === 0 || chosenBrand.some((B => B.toLowerCase() === item.brand.toLowerCase()))
@@ -18,6 +22,15 @@ function ProductsOrganisms() {
         const filterPrice = item.price >= priceRange[0] && item.price <= priceRange[1]
         return filterSeaarch && filterBrands && filterCategory && filterPrice
     })
+    useEffect(() => {
+        const url = new URLSearchParams()
+        if (valueInput) url.set('search', valueInput)
+        if (chosenBrand.length) url.set('brand', chosenBrand.join(','))
+        if (chosenCategory.length) url.set('category', chosenCategory.join(','))
+        url.set('min', priceRange[0].toString())
+        url.set('max', priceRange[1].toString())
+        router.push(`/products?${url.toString()}`,)
+    }, [valueInput, chosenBrand, chosenCategory, priceRange])
     return (
         <div className='w-full h-auto flex flex-col justify-center items-center'>
             <div className="w-300 h-30 flex items-center gap-5">
